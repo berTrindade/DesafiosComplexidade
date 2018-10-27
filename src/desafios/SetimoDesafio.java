f6483d4dc94ebfc77a0f3e14ed8152c7ba2a1100
Leonardo Borba <i866806@slel00681782a.amer.global.corp.sap>
2018-10-27 15:06:37 -0300

    finished SetimoDesafio

--- src/desafios/SetimoDesafio.java
+++ src/desafios/SetimoDesafio.java
@@ -6,10 +6,12 @@ import java.util.HashMap;
 
 public class SetimoDesafio {
 
+	private Estrutura base;
 	HashMap<String, Estrutura> estruturas = new HashMap<>();
 	
 	public SetimoDesafio(String[] info){
 		
+		boolean first = true;
 		for(String str : info){
 			String[] split = str.split(" ");
 			Estrutura estrutura = null;
@@ -17,8 +19,11 @@ public class SetimoDesafio {
 			if(estruturas.containsKey(estName)){
 				estrutura = estruturas.get(estName);
 			}else{
-				estrutura = new Estrutura(estName);
+				estrutura = new Estrutura(estName, first);
 				estruturas.put(estName, estrutura);
+				if(first)
+					base = estrutura;
+				first = false;
 			}
 			int tamanho = (int) (Math.floor(( Double.parseDouble(split[1])/2.0)));
 			
@@ -26,12 +31,13 @@ public class SetimoDesafio {
 			for(int i = 0; i < tamanho;i++){
 				populate(estrutura.getLeft(), split[i]);
 			}
-			for(int i = tamanho+1; i < str.length()-1;i++){
+			for(int i = tamanho+1; i < split.length;i++){
 				populate(estrutura.getRight(), split[i]);
 			}
 			estrutura.setMeio(decideEstrutura(split[tamanho]));
 		}
 		
+		base.calculateAsBase();
 		
 	}
 
@@ -40,17 +46,6 @@ public class SetimoDesafio {
 		lado.add(inner);
 	
 	}
-
-
-
-
-
-
-
-
-
-
-
 
 	private Estrutura decideEstrutura(String nomeValor) {
 		Estrutura inner = null;
@@ -61,33 +56,66 @@ public class SetimoDesafio {
 				inner = estruturas.get(nomeValor);
 			}else{
 				inner = new Estrutura(nomeValor);
+				estruturas.put(nomeValor, inner);
 			}
 		}
 		return inner;
 	}
 
 
-
-
-
-
-
-
-
-
-
-
+	public void getStructures(){
+		for(String s : estruturas.keySet()){
+			System.out.println(s);
+		}
+	}
+	
 	class Estrutura{
 		private String name;
-		private int value;
+		private int value = 0;
 		private ArrayList<Estrutura> left = new ArrayList<>();
 		private ArrayList<Estrutura> right = new ArrayList<>();
 		private Estrutura meio;
-		private int momentum;
+		private boolean momentum = false;
 		
-		public Estrutura(String name, int momentum){
-			this.momentum = momentum;
+		public Estrutura(String name, boolean first){
+			this.momentum = first;
 			this.name = name;
+		}
+		public void calculateAsBase() {
+			int left= 0;
+			int right =0;
+			for( Estrutura e : this.left){
+				left += e.calculate() * ( this.left.size() - this.left.indexOf(e));
+			}
+			for( Estrutura e : this.right){
+				right += e.calculate() * (this.right.indexOf(e)+1);
+			}
+			if(left == right)
+				System.out.println("BASE: "+ name + " IS BALANCED");
+		}
+		public int calculate() {
+			
+				if(value > 0)
+					return value;
+				else{
+					int values = 0;
+					int left = 0;
+					int right = 0;
+					for( Estrutura e : this.left){
+						left += e.calculate();
+					}
+					values += left;
+					for( Estrutura e : this.right){
+						right += e.calculate();
+					}
+					values += right;
+					if(right == left){
+						System.out.println(name + " IS BALANCED");
+					}
+					values += meio.calculate();
+					return values;
+				}
+					
 		}
 		public Estrutura(String name){
 			this.name = name;
@@ -127,10 +155,10 @@ public class SetimoDesafio {
 		public void setRight(ArrayList<Estrutura> right) {
 			this.right = right;
 		}
-		public int getMomentum() {
+		public boolean getMomentum() {
 			return momentum;
 		}
-		public void setMomentum(int momentum) {
+		public void setMomentum(boolean momentum) {
 			this.momentum = momentum;
 		}
 	}
